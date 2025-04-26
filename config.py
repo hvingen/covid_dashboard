c057e3da1668f5ddd21e43d05b2789b5b273c62b
Harold van Ingen <harold.van.ingen@isb.nl>
2025-04-26 20:10:33 +0200

    Codebase-en-notebook-update
    De codebase van de laatste werkende versie ingevoegd. Inclusief het Dashboard met de heatmap

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.resolve()
 SRC_DIR = PROJECT_ROOT / "src"
-SHAPEFILES_DIR = PROJECT_ROOT / "data" / "shapefiles"
-
-# Aangepast CSV_FOLDER naar CSV_DIR
-CSV_DIR = PROJECT_ROOT / "data" / "csv"
-EXPORT_FOLDER = PROJECT_ROOT / "notebooks" / "exports"
+NOTEBOOKS_DIR = PROJECT_ROOT / 'notebooks'
+SHAPEFILES_DIR = PROJECT_ROOT / 'data' / 'shapefiles'
+CSV_DIR = PROJECT_ROOT / 'data' / 'csv'
+EXPORT_DIR = PROJECT_ROOT / 'notebooks' / 'exports'
